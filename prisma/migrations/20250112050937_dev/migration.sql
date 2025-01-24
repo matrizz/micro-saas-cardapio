2418b46560254275ce175fbf340a30829a58942c
matrizz <95687945+matrizz@users.noreply.github.com>
2025-01-23 21:41:39 -0300

    Delete prisma/migrations directory

--- prisma/migrations/20250112050937_dev/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Menu` (
-    `sectionName` VARCHAR(191) NOT NULL,
-    `items` JSON NOT NULL,
-
-    UNIQUE INDEX `Menu_sectionName_key`(`sectionName`),
-    PRIMARY KEY (`sectionName`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
